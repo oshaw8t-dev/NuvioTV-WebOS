f80f94a9a2980510ecf0cdc4e9f5f0c6efd96b81
oshaw8t-dev <oshaw8@gmail.com>
2026-03-05 17:08:19 +0100

    fix: addon-manager hosted on GitHub Pages

---
+++ js/config.js
@@ -0,0 +1,10 @@
+export const SUPABASE_URL = 'https://xuonpifwlrutnnzisovs.supabase.co';
+export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh1b25waWZ3bHJ1dG5uemlzb3ZzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIwMzU5MTEsImV4cCI6MjA4NzYxMTkxMX0.3d_GgCEcx3D1qq0v83ozJCEQSz4JjqtGD3t_CL395qU';
+
+export const TMDB_API_KEY = '3af97fc09118b424c8ce185438febc27';
+export const TMDB_READ_ACCESS_TOKEN = '';
+
+export const TV_LOGIN_REDIRECT_BASE_URL = 'https://oshaw8t-dev.github.io/NuvioTV-WebOS/addon-manager';
+
+export const APP_VERSION = '0.1.0';
+export const DEBUG = true;
